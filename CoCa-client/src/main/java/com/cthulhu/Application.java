edb2dc28c0d979638a6d9d9e32ebb2399dcf5266
M4hler <oskar.makos.makowski@gmail.com>
2024-01-28 21:49:43 +0100

    Created basic login page.

--- CoCa-client/src/main/java/com/cthulhu/Application.java
+++ CoCa-client/src/main/java/com/cthulhu/Application.java
@@ -1,21 +1,63 @@
 package com.cthulhu;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
+    public void start(Stage stage) {
+        stage.setTitle("Login");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        stage.setScene(new Scene(root, 300, 250));
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text title = new Text("Welcome to CoCa client");
+        title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(title, 0, 0, 2, 1);
+
+        Label userName = new Label("User name:");
+        grid.add(userName, 0, 1);
+
+        TextField userNameTextField = new TextField();
+        grid.add(userNameTextField, 1, 1);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0, 2);
+
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
+
+        Button loginButton = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(loginButton);
+        grid.add(hbBtn, 1, 4);
+
+        Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
+        loginButton.setOnAction(e -> {
+            actionTarget.setFill(Color.FIREBRICK);
+            actionTarget.setText("Sign in button pressed");
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
+        stage.setScene(scene);
         stage.show();
     }
 }
