208e854184b92fe303ab4e2e22130179b1240aeb
Mahler <oskar.makos.makowski@gmail.com>
2024-03-19 22:31:43 +0100

    Concatenating password with salt and hashing with SHA-512.

--- CoCa-server/src/main/java/com/cthulhu/controllers/RegistrationController.java
+++ CoCa-server/src/main/java/com/cthulhu/controllers/RegistrationController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 @RestController
@@ -19,14 +22,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/register")
-    public HttpStatus register(@RequestBody RegistrationData registrationData) {
-        System.out.format("Received %s %s\n", registrationData.getName(), registrationData.getPassword());
-
+    public HttpStatus register(@RequestBody RegistrationData registrationData) throws NoSuchAlgorithmException {
         if(accountService.userExists(registrationData.getName())) {
             return HttpStatus.CONFLICT;
         }
 
-        Account account = new Account(registrationData.getName(), registrationData.getPassword(), UUID.randomUUID().toString().substring(0, 16));
+        String salt = UUID.randomUUID().toString().substring(0, 16);
+        String toHash = registrationData.getPassword() + salt;
+
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+        messageDigest.update(toHash.getBytes());
+        byte[] hashedPassword = messageDigest.digest();
+        String password = new String(hashedPassword, StandardCharsets.UTF_8).substring(0, 64);
+
+        Account account = new Account(registrationData.getName(), password, salt);
         accountService.save(account);
 
         return HttpStatus.OK;
