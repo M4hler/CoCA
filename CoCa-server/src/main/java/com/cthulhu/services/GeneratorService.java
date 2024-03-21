2de73ef1755745e1b99582b847f5cc132e761dca
Mahler <oskar.makos.makowski@gmail.com>
2024-03-21 22:55:56 +0100

    Added GeneratorService.

---
+++ CoCa-server/src/main/java/com/cthulhu/services/GeneratorService.java
@@ -0,0 +1,18 @@
+package com.cthulhu.services;
+
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+
+@Service
+public class GeneratorService {
+    private final SecureRandom generator;
+
+    public GeneratorService() {
+        generator = new SecureRandom();
+    }
+
+    public int rollDie(int bound) {
+        return generator.nextInt(bound) + 1;
+    }
+}
