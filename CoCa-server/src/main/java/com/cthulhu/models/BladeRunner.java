f2f4c9f3f595a7f5c1d0a3f5df70440a7f3ed772
Mahler <oskar.makos.makowski@gmail.com>
2024-04-21 14:44:54 +0200

    Expanded BladeRunner class with skill values.

--- CoCa-server/src/main/java/com/cthulhu/models/BladeRunner.java
+++ CoCa-server/src/main/java/com/cthulhu/models/BladeRunner.java
@@ -15,12 +15,28 @@ public class BladeRunner {
     private boolean secretReplicant;
     private String archetype;
     private int yearsOnTheForce;
+
     private int strength;
     private int agility;
     private int intelligence;
     private int empathy;
+
     private int health;
     private int resolve;
+
+    private int force;
+    private int handToHandCombat;
+    private int stamina;
+    private int firearms;
+    private int mobility;
+    private int stealth;
+    private int medicalAid;
+    private int observation;
+    private int tech;
+    private int connections;
+    private int manipulation;
+    private int insight;
+
     private int promotionPoints;
     private int humanityPoints;
     private int chinyenPoints;
