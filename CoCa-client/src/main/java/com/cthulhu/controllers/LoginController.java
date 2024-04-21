cc82331845369d04a946dba23cdcc6d3b8d23af1
Mahler <oskar.makos.makowski@gmail.com>
2024-04-21 23:30:54 +0200

    Error management on login page.

--- CoCa-client/src/main/java/com/cthulhu/controllers/LoginController.java
+++ CoCa-client/src/main/java/com/cthulhu/controllers/LoginController.java
@@ -5,6 +5,7 @@ import com.cthulhu.views.LoginView;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.scene.paint.Color;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.net.URI;
@@ -14,6 +15,7 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 @Getter
 public class LoginController {
@@ -24,24 +26,59 @@ public class LoginController {
     }
 
     private void loginAction() {
-        loginView.getActionTarget().setFill(Color.FIREBRICK);
-        loginView.getActionTarget().setText("Sign in button pressed");
+        String name = loginView.getNameTextField().getText();
+        String password = loginView.getPasswordField().getText();
+
+        if(name.length() < 4) {
+            setErrorMessage("Name must be at least 4 characters long");
+            return;
+        }
+
+        if(password.length() < 8) {
+            setErrorMessage("Password must be at least 8 characters long");
+            return;
+        }
 
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-512");
-            byte[] hashBytes = digest.digest(loginView.getPasswordField().getText().getBytes(StandardCharsets.UTF_8));
+            byte[] hashBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
             String hash = new String(hashBytes, StandardCharsets.UTF_8);
 
-            LoginData data = new LoginData(loginView.getNameTextField().getText(), hash);
+            LoginData data = new LoginData(name, hash);
             URI uri = URI.create("http://127.0.0.1:8080/register");
             HttpClient client = HttpClient.newHttpClient();
             String payload = new ObjectMapper().writeValueAsString(data);
-            HttpRequest request = HttpRequest.newBuilder().uri(uri).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(payload)).build();
+            HttpRequest request = HttpRequest.newBuilder().uri(uri)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(payload)).build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Status: " + response.statusCode());
+            String status = response.body().replaceAll("\"", "");
+
+            if(Objects.equals(status, HttpStatus.CONFLICT.name())) {
+                setErrorMessage("User with this name already exists");
+                return;
+            }
+
+            if(!Objects.equals(status, HttpStatus.OK.name())) {
+                setErrorMessage("Server responded with error, code: " + response.statusCode());
+                return;
+            }
+
+            setErrorMessage("Everything good");
         }
-        catch (NoSuchAlgorithmException | InterruptedException | IOException ex) {
-            throw new RuntimeException(ex);
+        catch(NoSuchAlgorithmException e) {
+            setErrorMessage("Wrong algorithm used to hash the password");
         }
+        catch(InterruptedException e) {
+            setErrorMessage("Communication with server was interrupted");
+        }
+        catch(IOException ex) {
+            setErrorMessage("Server isn't responding");
+        }
+    }
+
+    private void setErrorMessage(String message) {
+        loginView.getActionTarget().setFill(Color.FIREBRICK);
+        loginView.getActionTarget().setText(message);
     }
 }
