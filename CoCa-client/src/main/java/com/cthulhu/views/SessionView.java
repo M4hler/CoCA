ed583307bfa18a9c697ea5b15dc25ad1e4a2f1a6
Mahler <oskar.makos.makowski@gmail.com>
2024-05-26 21:59:27 +0200

    Created map method to display numeric values as letters.

--- CoCa-client/src/main/java/com/cthulhu/views/SessionView.java
+++ CoCa-client/src/main/java/com/cthulhu/views/SessionView.java
@@ -51,70 +51,70 @@ public class SessionView implements IView {
 
         Label strengthLabel = createLabel("Strength", 14);
         grid.add(strengthLabel, 0, 2, 2, 1);
-        strength = createLabel(bladeRunner.getStrength(), 14);
+        strength = createLabel(map(bladeRunner.getStrength()), 14);
         grid.add(strength, 2, 2, 1, 1);
         Label forceLabel = createLabel("Force", 12);
         grid.add(forceLabel, 0, 3, 2, 1);
-        force = createLabel(bladeRunner.getForce(), 12);
+        force = createLabel(map(bladeRunner.getForce()), 12);
         grid.add(force, 2, 3, 1, 1);
         Label handToHandCombatLabel = createLabel("Hand-to-hand combat", 12);
         grid.add(handToHandCombatLabel, 0, 4, 2, 1);
-        handToHandCombat = createLabel(bladeRunner.getHandToHandCombat(), 12);
+        handToHandCombat = createLabel(map(bladeRunner.getHandToHandCombat()), 12);
         grid.add(handToHandCombat, 2, 4, 1, 1);
         Label staminaLabel = createLabel("Stamina", 12);
         grid.add(staminaLabel, 0, 5, 2, 1);
-        stamina = createLabel(bladeRunner.getStamina(), 12);
+        stamina = createLabel(map(bladeRunner.getStamina()), 12);
         grid.add(stamina, 2, 5, 1, 1);
 
         Label agilityLabel = createLabel("Agility", 14);
         grid.add(agilityLabel, 0, 6, 2, 1);
-        agility = createLabel(bladeRunner.getAgility(), 14);
+        agility = createLabel(map(bladeRunner.getAgility()), 14);
         grid.add(agility, 2, 6, 1, 1);
         Label firearmsLabel = createLabel("Firearms", 12);
         grid.add(firearmsLabel, 0, 7, 2, 1);
-        firearms = createLabel(bladeRunner.getFirearms(), 12);
+        firearms = createLabel(map(bladeRunner.getFirearms()), 12);
         grid.add(firearms, 2, 7, 1, 1);
         Label mobilityLabel = createLabel("Mobility", 12);
         grid.add(mobilityLabel, 0, 8, 2, 1);
-        mobility = createLabel(bladeRunner.getMobility(), 12);
+        mobility = createLabel(map(bladeRunner.getMobility()), 12);
         grid.add(mobility, 2, 8, 1, 1);
         Label stealthLabel = createLabel("Stealth", 12);
         grid.add(stealthLabel, 0, 9, 2, 1);
-        stealth = createLabel(bladeRunner.getStealth(), 12);
+        stealth = createLabel(map(bladeRunner.getStealth()), 12);
         grid.add(stealth, 2, 9, 1, 1);
 
         Label intelligenceLabel = createLabel("Intelligence", 14);
         grid.add(intelligenceLabel, 0, 10, 2, 1);
-        intelligence = createLabel(bladeRunner.getIntelligence(), 14);
+        intelligence = createLabel(map(bladeRunner.getIntelligence()), 14);
         grid.add(intelligence, 2, 10, 1, 1);
         Label medicalAidLabel = createLabel("Medical aid", 12);
         grid.add(medicalAidLabel, 0, 11, 2, 1);
-        medicalAid = createLabel(bladeRunner.getMedicalAid(), 12);
+        medicalAid = createLabel(map(bladeRunner.getMedicalAid()), 12);
         grid.add(medicalAid, 2, 11, 2, 1);
         Label observationLabel = createLabel("Observation", 12);
         grid.add(observationLabel, 0, 12, 2, 1);
-        observation = createLabel(bladeRunner.getObservation(), 12);
+        observation = createLabel(map(bladeRunner.getObservation()), 12);
         grid.add(observation, 2, 12, 2, 1);
         Label techLabel = createLabel("Tech", 12);
         grid.add(techLabel, 0, 13, 2, 1);
-        tech = createLabel(bladeRunner.getTech(), 12);
+        tech = createLabel(map(bladeRunner.getTech()), 12);
         grid.add(tech, 2, 13, 1, 1);
 
         Label empathyLabel = createLabel("Empathy", 14);
         grid.add(empathyLabel, 0, 14, 2, 1);
-        empathy = createLabel(bladeRunner.getEmpathy(), 14);
+        empathy = createLabel(map(bladeRunner.getEmpathy()), 14);
         grid.add(empathy, 2, 14, 1, 1);
         Label connectionsLabel = createLabel("Connections", 12);
         grid.add(connectionsLabel, 0, 15, 2, 1);
-        connections = createLabel(bladeRunner.getConnections(), 12);
+        connections = createLabel(map(bladeRunner.getConnections()), 12);
         grid.add(connections, 2, 15, 1, 1);
         Label insightLabel = createLabel("Insight", 12);
         grid.add(insightLabel, 0, 16, 2, 1);
-        insight = createLabel(bladeRunner.getInsight(), 12);
+        insight = createLabel(map(bladeRunner.getInsight()), 12);
         grid.add(insight, 2, 16, 1, 1);
         Label manipulationLabel = createLabel("Manipulation", 12);
         grid.add(manipulationLabel, 0, 17, 2, 1);
-        manipulation = createLabel(bladeRunner.getManipulation(), 12);
+        manipulation = createLabel(map(bladeRunner.getManipulation()), 12);
         grid.add(manipulation, 2, 17, 1, 1);
 
         VBox vBox = new VBox();
@@ -171,10 +171,23 @@ public class SessionView implements IView {
         return label;
     }
 
-    private Label createLabel(int value, int size) {
-        Label label =  new Label(String.valueOf(value));
-        label.setFont(Font.font("Tahoma", FontWeight.NORMAL, size));
-        label.setAlignment(Pos.CENTER_LEFT);
-        return label;
+    private String map(int value) {
+        switch (value) {
+            case 6 -> {
+                return "D";
+            }
+            case 8 -> {
+                return "C";
+            }
+            case 10 -> {
+                return "B";
+            }
+            case 12 -> {
+                return "A";
+            }
+            default -> {
+                return "?";
+            }
+        }
     }
 }
