72bfae7c92fabe316e6aa7ff829f2495887df231
Mahler <oskar.makos.makowski@gmail.com>
2024-09-07 15:08:25 +0200

    Refactored SessionView, added background change when mouse is over skill labels.

--- CoCa-client/src/main/java/com/cthulhu/views/SessionView.java
+++ CoCa-client/src/main/java/com/cthulhu/views/SessionView.java
@@ -5,14 +5,15 @@ import com.cthulhu.events.RollEvent;
 import com.cthulhu.models.BladeRunner;
 import jakarta.jms.ConnectionFactory;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxTreeCell;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -31,23 +32,6 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 @EnableJms
 public class SessionView implements IView {
-    private Label strength;
-    private Label force;
-    private Label handToHandCombat;
-    private Label stamina;
-    private Label agility;
-    private Label firearms;
-    private Label mobility;
-    private Label stealth;
-    private Label intelligence;
-    private Label medicalAid;
-    private Label observation;
-    private Label tech;
-    private Label empathy;
-    private Label connections;
-    private Label insight;
-    private Label manipulation;
-
     private Text title;
     private CheckBoxTreeItem<String> root;
     private final GridPane grid;
@@ -115,73 +99,25 @@ public class SessionView implements IView {
         Label stats = createLabel("Stats", 16);
         grid.add(stats, 0, 1, 2, 1);
 
-        Label strengthLabel = createLabel("Strength", 14);
-        grid.add(strengthLabel, 0, 2, 2, 1);
-        strength = createLabel(map(bladeRunner.getStrength()), 14);
-        grid.add(strength, 2, 2, 1, 1);
-        Label forceLabel = createLabel("Force", 12);
-        grid.add(forceLabel, 0, 3, 2, 1);
-        force = createLabel(map(bladeRunner.getForce()), 12);
-        grid.add(force, 2, 3, 1, 1);
-        Label handToHandCombatLabel = createLabel("Hand-to-hand combat", 12);
-        grid.add(handToHandCombatLabel, 0, 4, 2, 1);
-        handToHandCombat = createLabel(map(bladeRunner.getHandToHandCombat()), 12);
-        grid.add(handToHandCombat, 2, 4, 1, 1);
-        Label staminaLabel = createLabel("Stamina", 12);
-        grid.add(staminaLabel, 0, 5, 2, 1);
-        stamina = createLabel(map(bladeRunner.getStamina()), 12);
-        grid.add(stamina, 2, 5, 1, 1);
+        createRow("Strength", bladeRunner.getStrength(), 2, 14);
+        createRow("Force", bladeRunner.getForce(), 3, 12);
+        createRow("Hand-to-hand combat", bladeRunner.getHandToHandCombat(), 4, 12);
+        createRow("Stamina", bladeRunner.getStamina(), 5, 12);
 
-        Label agilityLabel = createLabel("Agility", 14);
-        grid.add(agilityLabel, 0, 6, 2, 1);
-        agility = createLabel(map(bladeRunner.getAgility()), 14);
-        grid.add(agility, 2, 6, 1, 1);
-        Label firearmsLabel = createLabel("Firearms", 12);
-        grid.add(firearmsLabel, 0, 7, 2, 1);
-        firearms = createLabel(map(bladeRunner.getFirearms()), 12);
-        grid.add(firearms, 2, 7, 1, 1);
-        Label mobilityLabel = createLabel("Mobility", 12);
-        grid.add(mobilityLabel, 0, 8, 2, 1);
-        mobility = createLabel(map(bladeRunner.getMobility()), 12);
-        grid.add(mobility, 2, 8, 1, 1);
-        Label stealthLabel = createLabel("Stealth", 12);
-        grid.add(stealthLabel, 0, 9, 2, 1);
-        stealth = createLabel(map(bladeRunner.getStealth()), 12);
-        grid.add(stealth, 2, 9, 1, 1);
+        createRow("Agility", bladeRunner.getAgility(), 6, 14);
+        createRow("Firearms", bladeRunner.getFirearms(), 7, 12);
+        createRow("Mobility", bladeRunner.getMobility(), 8, 12);
+        createRow("Stealth", bladeRunner.getStealth(), 9, 12);
 
-        Label intelligenceLabel = createLabel("Intelligence", 14);
-        grid.add(intelligenceLabel, 0, 10, 2, 1);
-        intelligence = createLabel(map(bladeRunner.getIntelligence()), 14);
-        grid.add(intelligence, 2, 10, 1, 1);
-        Label medicalAidLabel = createLabel("Medical aid", 12);
-        grid.add(medicalAidLabel, 0, 11, 2, 1);
-        medicalAid = createLabel(map(bladeRunner.getMedicalAid()), 12);
-        grid.add(medicalAid, 2, 11, 2, 1);
-        Label observationLabel = createLabel("Observation", 12);
-        grid.add(observationLabel, 0, 12, 2, 1);
-        observation = createLabel(map(bladeRunner.getObservation()), 12);
-        grid.add(observation, 2, 12, 2, 1);
-        Label techLabel = createLabel("Tech", 12);
-        grid.add(techLabel, 0, 13, 2, 1);
-        tech = createLabel(map(bladeRunner.getTech()), 12);
-        grid.add(tech, 2, 13, 1, 1);
+        createRow("Intelligence", bladeRunner.getIntelligence(), 10, 14);
+        createRow("Medical aid", bladeRunner.getMedicalAid(), 11, 12);
+        createRow("Observation", bladeRunner.getObservation(), 12, 12);
+        createRow("Tech", bladeRunner.getTech(), 13, 12);
 
-        Label empathyLabel = createLabel("Empathy", 14);
-        grid.add(empathyLabel, 0, 14, 2, 1);
-        empathy = createLabel(map(bladeRunner.getEmpathy()), 14);
-        grid.add(empathy, 2, 14, 1, 1);
-        Label connectionsLabel = createLabel("Connections", 12);
-        grid.add(connectionsLabel, 0, 15, 2, 1);
-        connections = createLabel(map(bladeRunner.getConnections()), 12);
-        grid.add(connections, 2, 15, 1, 1);
-        Label insightLabel = createLabel("Insight", 12);
-        grid.add(insightLabel, 0, 16, 2, 1);
-        insight = createLabel(map(bladeRunner.getInsight()), 12);
-        grid.add(insight, 2, 16, 1, 1);
-        Label manipulationLabel = createLabel("Manipulation", 12);
-        grid.add(manipulationLabel, 0, 17, 2, 1);
-        manipulation = createLabel(map(bladeRunner.getManipulation()), 12);
-        grid.add(manipulation, 2, 17, 1, 1);
+        createRow("Empathy", bladeRunner.getEmpathy(), 14, 14);
+        createRow("Connections", bladeRunner.getConnections(), 15, 12);
+        createRow("Insight", bladeRunner.getInsight(), 16, 12);
+        createRow("Manipulation", bladeRunner.getManipulation(), 17, 12);
     }
 
     private void setupAdmin() {
@@ -222,10 +158,28 @@ public class SessionView implements IView {
         jmsTemplate.convertAndSend(MainController.getQueue(), rollEvent);
     }
 
+    private void createRow(String text, int value, int row, int size) {
+        var label = createClickableLabel(text, size);
+        var valueLabel = createLabel(map(value), size);
+        grid.add(label, 0, row, 2, 1);
+        grid.add(valueLabel, 2, row, 1, 1);
+    }
+
     private Label createLabel(String s, int size) {
         Label label =  new Label(s);
         label.setFont(Font.font("Tahoma", FontWeight.NORMAL, size));
         label.setAlignment(Pos.CENTER_LEFT);
+        return label;
+    }
+
+    private Label createClickableLabel(String s, int size) {
+        Label label =  new Label(s);
+        label.setFont(Font.font("Tahoma", FontWeight.NORMAL, size));
+        label.setAlignment(Pos.CENTER_LEFT);
+        label.setOnMouseEntered(mouseEvent
+                -> label.setBackground(new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY))));
+        label.setOnMouseExited(mouseEvent -> label.setBackground(Background.EMPTY));
+        //label.setOnMouseClicked();
         return label;
     }
 
