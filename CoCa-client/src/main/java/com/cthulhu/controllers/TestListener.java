e9935431f61832ebe6c40f10dc6b212320d49040
Mahler <oskar.makos.makowski@gmail.com>
2024-09-08 23:29:49 +0200

    Rewrote tryParse to match server's.

--- CoCa-client/src/main/java/com/cthulhu/controllers/TestListener.java
+++ CoCa-client/src/main/java/com/cthulhu/controllers/TestListener.java
@@ -1,6 +1,7 @@
 package com.cthulhu.controllers;
 
 import com.cthulhu.events.BladeRunnerDataEvent;
+import com.cthulhu.events.Event;
 import com.cthulhu.events.JoinEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,36 +24,34 @@ public class TestListener implements MessageListener {
         try {
             System.out.println("onMessage in javafx application");
             String body = message.getBody(String.class);
-            if(tryParse(body, JoinEvent.class)) {
-                var event = mapper.readValue(body, JoinEvent.class);
+            var event = tryParse(body, JoinEvent.class);
+            if(event != null) {
                 System.out.println("JoinEvent: " + event.getName());
                 if(controller != null) {
                     controller.test(event.getName());
                 }
+                return;
             }
-            else if(tryParse(body, BladeRunnerDataEvent.class)) {
-                var event = mapper.readValue(body, BladeRunnerDataEvent.class);
-                if(event.getBladeRunner() != null) {
-                    System.out.println("BladeRunner data: " + event.getBladeRunner().getName());
-                    controller.test2(event.getBladeRunner().getName());
+
+            var event1 = tryParse(body, BladeRunnerDataEvent.class);
+            if(event1 != null) {
+                if(event1.getBladeRunner() != null) {
+                    System.out.println("BladeRunner data: " + event1.getBladeRunner().getName());
+                    controller.test2(event1.getBladeRunner().getName());
                 }
             }
         }
         catch (JMSException e) {
             e.printStackTrace();
         }
-        catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 
-    private boolean tryParse(String body, Class<?> clazz) {
+    private <T extends Event> T tryParse(String body, Class<T> clazz) {
         try {
-            mapper.readValue(body, clazz);
-            return true;
+            return mapper.readValue(body, clazz);
         }
         catch(JsonProcessingException e) {
-            return false;
+            return null;
         }
     }
 }
