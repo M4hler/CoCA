2de8aac710b04ef14b9e784646fefa7bfff6a08d
Mahler <oskar.makos.makowski@gmail.com>
2024-09-15 20:46:15 +0200

    Fixed CoCaListenerService - now it can instantiate classes with constructor arguments. Uncommented constructor in RollEventListener to test this.

--- CoCa-server/src/main/java/com/cthulhu/listeners/RollEventListener.java
+++ CoCa-server/src/main/java/com/cthulhu/listeners/RollEventListener.java
@@ -8,16 +8,16 @@ import org.springframework.stereotype.Service;
 @Service
 @CoCaListener(handles = RollEvent.class)
 public class RollEventListener extends CustomListener<RollEvent> {
-//    private final BladeRunnerService bladeRunnerService;
-//
-//    public RollEventListener(BladeRunnerService bladeRunnerService) {
-//        this.bladeRunnerService = bladeRunnerService;
-//    }
+    private final BladeRunnerService bladeRunnerService;
+
+    public RollEventListener(BladeRunnerService bladeRunnerService) {
+        this.bladeRunnerService = bladeRunnerService;
+    }
 
     @Override
     public void handle(RollEvent event) {
         System.out.println("Event: " + event.getBladeRunner() + " " + event.getSkill() + " " + event.getBonusDie());
-//        var bladeRunner = bladeRunnerService.getBladeRunner(event.getBladeRunner());
-//        System.out.println("Received: " + bladeRunner.getName() + " " + bladeRunner.getArchetype());
+        var bladeRunner = bladeRunnerService.getBladeRunner(event.getBladeRunner());
+        System.out.println("Received: " + bladeRunner.getName() + " " + bladeRunner.getArchetype());
     }
 }
--- CoCa-server/src/main/java/com/cthulhu/services/CoCaListenerService.java
+++ CoCa-server/src/main/java/com/cthulhu/services/CoCaListenerService.java
@@ -3,9 +3,9 @@ package com.cthulhu.services;
 import com.cthulhu.annotations.CoCaListener;
 import com.cthulhu.events.Event;
 import com.cthulhu.listeners.CustomListener;
-import jakarta.jms.MessageListener;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.stereotype.Service;
@@ -19,7 +19,8 @@ public class CoCaListenerService {
     @Getter
     private static final Map<Event, CustomListener<? extends Event>> cocaListeners = new HashMap<>();
 
-    public CoCaListenerService() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public CoCaListenerService(ApplicationContext context) throws ClassNotFoundException, NoSuchMethodException,
+            InvocationTargetException, InstantiationException, IllegalAccessException {
         var provider = new ClassPathScanningCandidateComponentProvider(false);
         provider.addIncludeFilter(new AnnotationTypeFilter(CoCaListener.class));
         var candidates = provider.findCandidateComponents("com.cthulhu");
@@ -32,10 +33,21 @@ public class CoCaListenerService {
                     var eventClass = Class.forName(eventName.toString().substring(6)); //strips 'class' prefix
                     var eventInstance = eventClass.getDeclaredConstructor().newInstance();
                     var listenerClass = Class.forName(candidate.getBeanClassName());
-                    var listenerInstance = listenerClass.getDeclaredConstructor().newInstance();
+                    var constructors = listenerClass.getConstructors();
+                    var constructor = constructors[0];
+                    for(var c : constructors) {
+                        if(c.getParameterCount() > constructor.getParameterCount()) {
+                            constructor = c;
+                        }
+                    }
+                    var parameters = constructor.getParameterTypes();
+                    Object[] beans = new Object[parameters.length];
+                    for(int i = 0; i < parameters.length; i++) {
+                        beans[i] = context.getBean(parameters[i]);
+                    }
+                    var listenerInstance = constructor.newInstance(beans);
 
                     if(listenerInstance instanceof CustomListener<?> listener && eventInstance instanceof Event event) {
-                        System.out.println("adding to map");
                         cocaListeners.put(event, listener);
                     }
                 }
