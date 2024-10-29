37518afe7f621fff749227b026a92eb487e5fe00
Mahler <oskar.makos.makowski@gmail.com>
2024-10-29 23:11:15 +0100

    Npc class in client.

---
+++ CoCa-client/src/main/java/com/cthulhu/models/Npc.java
@@ -0,0 +1,32 @@
+package com.cthulhu.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Npc {
+    private String name;
+    private boolean isHuman;
+
+    private int strength;
+    private int agility;
+    private int intelligence;
+    private int empathy;
+
+    private int health;
+    private int resolve;
+
+    private int force;
+    private int handToHandCombat;
+    private int stamina;
+    private int firearms;
+    private int mobility;
+    private int stealth;
+    private int medicalAid;
+    private int observation;
+    private int tech;
+    private int connections;
+    private int manipulation;
+    private int insight;
+}
