a0a4b38df445387e30e07952d643791409b330c2
Mahler <oskar.makos.makowski@gmail.com>
2024-11-14 23:10:31 +0100

    Copied model and event to client.

---
+++ CoCa-client/src/main/java/com/cthulhu/models/Mainframe.java
@@ -0,0 +1,12 @@
+package com.cthulhu.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Mainframe {
+    private int id;
+    private String title;
+    private String description;
+}
