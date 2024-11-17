37391fb7c4311bab4c423af0bb7e850dc0550e60
Mahler <oskar.makos.makowski@gmail.com>
2024-11-17 15:11:59 +0100

    Changed database type from text to varchar.

--- CoCa-server/src/main/java/com/cthulhu/models/Mainframe.java
+++ CoCa-server/src/main/java/com/cthulhu/models/Mainframe.java
@@ -11,8 +11,6 @@ public class Mainframe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    //@Lob
     private String title;
-    //@Lob
     private String description;
 }
