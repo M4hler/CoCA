a21c64918c281e90d0a3a36e31fbf7cbdd3e0105
Mahler <oskar.makos.makowski@gmail.com>
2024-12-20 23:38:08 +0100

    Added test dependencies.

---
+++ CoCa-server/src/test/java/com/coca/server/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.coca.server;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ExampleTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
